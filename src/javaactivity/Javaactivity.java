775a9fb5580fd6330c514b3a138e63a9bc9567a6
habalulu223 <jrreuben123@gmail.com>
2025-07-23 23:32:28 -0700

    added activity 1 to 3

--- src/javaactivity/Javaactivity.java
+++ src/javaactivity/Javaactivity.java
@@ -1,30 +1,31 @@
-
 package javaactivity;
+
 import java.util.Scanner;
+
 public class Javaactivity {
-Scanner scanner = new Scanner(System.in);
-   
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int arts;
-        String name = scanner.nextLine();
-        
-        
+        int arts, science, history, math, social;
+        String name;
+
+        System.out.print("Enter name: ");
+        name = in.nextLine();
+
         System.out.print("Enter marks in Science: ");
-        int science = in.nextInt();
+        science = in.nextInt(); 
         System.out.print("Enter marks in History: ");
-        int history = in.nextInt();
+        history = in.nextInt();
         System.out.print("Enter marks in Math: ");
-        int math = in.nextInt();
+        math = in.nextInt(); 
         System.out.print("Enter marks in Social: ");
-        int social= in.nextInt();
+        social = in.nextInt();
         System.out.print("Enter marks in Arts: ");
-        arts = in.nextInt();
-        
-        
+        arts = in.nextInt(); 
+
         int totalMarks = science + history + math + social + arts;
         double percentage = (totalMarks / 5.0);
-        
-        
+
         String remarks = "";
         if (percentage < 70) {
             remarks = "Fail";
@@ -39,18 +40,17 @@ Scanner scanner = new Scanner(System.in);
         } else {
             remarks = "Excellent";
         }
-        
-        // Display results
+
         System.out.println("Total Marks: " + totalMarks);
         System.out.println("Total percentage: " + percentage);
         System.out.println("Remarks: " + remarks);
-        
+
         if (percentage < 70) {
             System.out.println("Try Again");
         } else {
             System.out.println("Congrats " + name + " you Passed.");
         }
 
+        in.close();
     }
-    
 }
---
+++ src/javaactivity3/activiy3.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaactivity3;
+
+public class activiy3 {
+    Scanner scanner = new Scanner(System.in);
+        
+
+        System.out.print("Enter a number: ");
+        int number1 = scanner.nextInt();
+        if (number1 > 0) {
+            System.out.println(number1 + " is a positive number");
+        } else if (number1 < 0) {
+            System.out.println(number1 + " is a negative number");
+        } else {
+            System.out.println("Inputted number Zero");
+        }
+
+
+        System.out.print("Enter a number: ");
+        int number2 = scanner.nextInt();
+        if (number2 > 0) {
+            System.out.println(number2 + " is a positive number");
+        } else if (number2 < 0) {
+            System.out.println(number2 + " is a negative number");
+        } else {
+            System.out.println("Inputted number Zero");
+        }
+
+        
+        System.out.print("Enter a number: ");
+        int number3 = scanner.nextInt();
+        if (number3 > 0) {
+            System.out.println(number3 + " is a positive number");
+        } else if (number3 < 0) {
+            System.out.println(number3 + " is a negative number");
+        } else {
+            System.out.println("Inputted number Zero");
+        }
+
+        scanner.close();
+    }
+}
